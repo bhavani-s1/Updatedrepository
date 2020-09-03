a4df66a919446a93464be10be1bfcd655d02cc90
ATMECS\bhavani.kore <bhavani.kore@atmecs.com>
2020-09-03 16:47:43 +0530

    commit

--- src/main/java/Employeedetails/Employee.java
+++ src/main/java/Employeedetails/Employee.java
@@ -7,45 +7,48 @@ import java.util.Scanner;
 
 public class Employee {
 	
-	static Scanner scan=new Scanner(System.in);
-
+	static String id;
+	static String name;
+	static String department;
+	static String designation;
+	static Map<Object, List<String>> map;
+	static Scanner input = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		
-	static Map<Object, List<String>>map;
-
-		
-		int empid;
-		String ename;
-		String dept;
-		int sal;
 		int choice;
-		
 		map = new HashMap<Object, List<String>>();
 		
-		do{		
-			System.out.println("Enter your choice");
-			System.out.println("1.Add employee details");
-			System.out.println("2.Search employee");
-			System.out.println("3.Delete employee");
-			System.out.println("4.Display employee details");
-			System.out.println("5.Exit");
-			
-			choice=scan.nextInt();
-			
-		}
+		do {
+			System.out.println("Press 1 to Add Record");
+			System.out.println("Press 2 to Search Record");
+			System.out.println("Press 3 to Delete Record");
+			System.out.println("Press 4 to Display All record");
+			System.out.println("Press 5 to Exit");
+			choice = input.nextInt();
+			switch (choice) {
+			case 1:
+				AddEmployeeRecord add = new AddEmployeeRecord();
+				add.addRecord();
+				break;
+			case 2:
+				SearchEmployeeRecord search = new SearchEmployeeRecord();
+				search.searchRecord();
+				break;
+			case 3:
+				DeleteEmployeeRecord delete = new DeleteEmployeeRecord();
+				delete.deleteRecord();
+				break;
+			case 4:
+				DisplayEmployeeRecord display = new DisplayEmployeeRecord();
+				display.displayRecord();
+				break;
+			case 5:
+				System.out.println("You have pressed 5..The program has been ended...!!!");
+				break;
+			default:
+				System.out.println("Wrong Choice...!!");	
+			}
+		} while (choice != 5);
 		
-		switch(choice) {
-		
-		case 1:
-			Addemp add = new Addemp();
-			add.addRecord();
-			break;
-
-			
-		}
-		
-		
-
 	}
-
 }
