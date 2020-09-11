0343cc60c6897985b0274f4ac95fd39c50e57a6c
ATMECS\bhavani.kore <bhavani.kore@atmecs.com>
2020-09-11 21:46:22 +0530

    commit

--- src/main/java/TestngBasics1/Locators.java
+++ src/main/java/TestngBasics1/Locators.java
@@ -15,6 +15,25 @@ public class Locators {
 		driver.get("https://www.amazon.com/");
 		
 		driver.findElement(By.linkText("Sign in")).click();
+		
+		//Locators
+		//1.Id
+		
+		driver.findElement(By.id("ap_email")).sendKeys("bhavani.kore@amazon.com");
+		driver.findElement(By.id("continue")).click();
+		
+		
+	//2.Name
+		
+		driver.get("https://login.yahoo.com/");
+		
+		driver.findElement(By.name("username")).sendKeys("k.bhavani@yahoo.com");
+		
+				
+		
+		
+		
+		
 	}
 
 }
